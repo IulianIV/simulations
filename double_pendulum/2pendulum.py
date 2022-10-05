885dfcc524a9a22379bd114212ecc7d24f5ed1b6
Iulian Ichim <ichim.iulian12@gmail.com>
2022-10-06 01:06:22 +0300

    reduced code duplication

--- double_pendulum/2pendulum.py
+++ double_pendulum/2pendulum.py
@@ -1,19 +1,20 @@
 import pygame
-import math
+from assets.core import Simulation
 from pendulum import DoublePendulum, PI
 from utils import SCREEN_SIZE, BLACK, RED, GRAVITY, WHITE, CENTER_WIDTH, Y_OFFSET
 
 pygame.init()
 
-screen = pygame.display.set_mode(SCREEN_SIZE)
-trace_screen = pygame.display.set_mode(SCREEN_SIZE)
+# pendulum_font = pygame.font.SysFont('Times New Roman', 18)
+#
+# screen = pygame.display.set_mode(SCREEN_SIZE)
+#
+# pygame.display.set_caption("Double pendulum")
+#
+# clock = pygame.time.Clock()
 
-pygame.display.set_caption("Double pendulum")
-
-clock = pygame.time.Clock()
-
-p1 = DoublePendulum(BLACK, RED, 200, 20, 10, PI / 2)
-p2 = DoublePendulum(BLACK, RED, 100, 30, 5, PI / 8)
+p1 = DoublePendulum(BLACK, RED, 350, 20, 2, PI / 2)
+p2 = DoublePendulum(BLACK, RED, 130, 20, 10, PI / 2)
 
 
 """
@@ -23,6 +24,12 @@ p1 = DoublePendulum(BLACK, RED, 200, 20, 10, PI / 2)
 p2 = DoublePendulum(BLACK, RED, 100, 30, 5, PI / 8)
 
 """
+
+class PendulumSimulation(Simulation):
+
+    def __init__(self):
+        super().__init__()
+        pass
 
 
 def run_pendulum():
@@ -34,29 +41,34 @@ def run_pendulum():
                 done = True
 
         screen.fill(WHITE)
-        trace_screen.fill(WHITE)
 
         p1.p1_motion_model(GRAVITY, p2)
         p2.p2_motion_model(GRAVITY, p1)
 
-        # implement the update_y function
-        p1.x = p1.length * math.sin(p1.angle) + CENTER_WIDTH
-        p1.y = p1.length * math.cos(p1.angle) + Y_OFFSET
+        # implement the calculate_new_y function
+        p1.calculate_new_x(CENTER_WIDTH)
+        p1.calculate_new_y(Y_OFFSET)
 
-        p2.x = p1.x + p2.length * math.sin(p2.angle)
-        p2.y = p1.y + p2.length * math.cos(p2.angle)
+        p2.calculate_new_x(pendulum=p1)
+        p2.calculate_new_y(pendulum=p1)
+
+        p1.update_velocity()
+        p2.update_velocity()
+
+        p1.update_angle()
+        p2.update_angle()
 
         """
         ===== DRAW SECTION ====
         """
 
+        display_text = pendulum_font.render(p1.get_data('x'), False, (0, 0, 0))
+        screen.blit(display_text, (0, 0))
+
+
+
         p1.draw(screen, [CENTER_WIDTH, Y_OFFSET], [p1.x, p1.y])
         p2.draw(screen, [p1.x, p1.y], [p2.x, p2.y])
-
-        p1.velocity += p1.acceleration
-        p2.velocity += p2.acceleration
-        p1.angle += p1.velocity
-        p2.angle += p2.velocity
 
         pygame.display.update()
 
