72783e99a320e841482da6b109263da4cba3f8dc
IulianIV <ichim.iulian12@gmail.com>
2022-10-06 17:55:43 +0300

    Added data print pipeline for core simulation class

--- engine/core.py
+++ engine/core.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from typing import Any
 import pygame
 from utils import Colors, Screen, Fonts
 
@@ -9,12 +10,15 @@ pygame.font.init()
 class Simulation(ABC):
 
     def __init__(self, screen_size: tuple = Screen.SCREEN_SIZE, name: str = 'Simulation',
-                 font: str = Fonts.STANDARD_FONT, background: tuple = Colors.WHITE):
+                 background: tuple = Colors.WHITE):
         self.clock = pygame.time.Clock()
+
         self.screen_size = screen_size
         self._screen = self._set_screen()
-        self._font_size = 10
-        self.font = pygame.font.SysFont(font, self._font_size)
+
+        self.font_size = self._font_size
+        self.font_type = self._font_type
+
         self.background = background
         pygame.display.set_caption(name)
 
@@ -23,11 +27,33 @@ class Simulation(ABC):
 
         return _screen
 
-    def font_size(self, font_size: int):
-        self._font_size = font_size
+    @property
+    def _font_size(self):
+        size = 10
+        return size
+
+    @_font_size.setter
+    def _font_size(self, font_size: int):
+        self.font_size = font_size
+
+    @property
+    def _font_type(self):
+        font_type = Fonts.STANDARD_FONT
+
+        return font_type
+
+    @_font_type.setter
+    def _font_type(self, font_type: str):
+        self.font_type = font_type
 
     def set_background(self):
         self._screen.fill(self.background)
+
+    def display_data(self, data: Any, txt_antialias: bool = False,
+                     txt_color: Any = Colors.BLACK, txt_destination: tuple = (0, 0), txt_background: Any = None):
+        font = pygame.font.SysFont(self.font_type, self.font_size)
+        display_text = font.render(data, txt_antialias, txt_color, txt_background)
+        self._screen.blit(display_text, txt_destination)
 
     @abstractmethod
     def run(self):
